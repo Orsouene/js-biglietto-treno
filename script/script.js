b048505e3527144f517a2f89383933945b4e3fd4
Orsouene <orsoueneelaouizeb@gmail.com>
2024-10-28 16:29:57 +0100

    prezzo per passagero minorenne

--- script/script.js
+++ script/script.js
@@ -1,5 +1,16 @@
+// CONST
 const prezzo = 0.21;
 const scontoMinorenne = 20;
 const scontoSenior = 40;
+
+// let
 let prezzoFinale;
-let etàPasseggero;
+let etàPasseggero = parseInt(prompt("inserici l'età del passagero"));
+let distanzaPercorsa = parseInt(prompt("inserici la distanza percorsa"));
+let prezzoSenzaSconto = distanzaPercorsa * prezzo;
+// Condizione
+if (etàPasseggero <= 18) {
+  prezzoFinale =
+    prezzoSenzaSconto - (prezzoSenzaSconto * scontoMinorenne) / 100;
+}
+console.log(prezzoFinale);
